022554de24f09372e68f0fe4ccc3003b55c016ed
Abhishek Kumar <abhishek.kr.11511@gmail.com>
2024-01-29 21:13:26 +0530

    Added button component

---
+++ components/CustomButton.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { useState } from "react"
+
+const CustomButton = () => {
+
+
+    
+  return (
+    <button
+        disabled={false}
+        type="button"
+        className={`custom-btn`}
+        onClick={()=>{}}
+    >
+        View
+    </button>
+  )
+}
+export default CustomButton
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,22 @@
 import Image from "next/image"
 
+import CustomButton from "@/components/CustomButton"
+
+
 const Hero = () => {
   return (
-    <div>Hero Component</div>
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero__title">
+                Find, Book, or rent a Car : Quickly and easily!
+            </h1>
+            <p className="hero__subtitle">
+                Streamline your car rental experience with you effortless booking process.
+            </p>
+            
+            <CustomButton/>
+        </div>
+    </div>
   )
 }
 export default Hero
